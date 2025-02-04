ebabe2db5f0f71386b2b2eef5b52ab66fad3b775
Cdev-99 <c95683+1@gmail.com>
2025-02-04 10:41:56 -0700

    again

--- ai.js
+++ ai.js
@@ -242,7 +242,7 @@
         input.right = desiredOutput[3] > 0.5;
         input.field = desiredOutput[4] > 0.5;
         input.fire = desiredOutput[5] > 0.5;  
-        input.angle = output[6] * Math.PI * 2 * 180;
+        input.angle = output[6] * Math.PI * 2;
         m.look = function () {
             m.angle = input.angle
             return input.angle};
